510e68b67e6fdf72e84892273b8542c323dca4a6
hellnp <johnngure086@gmail.com>
2023-03-16 14:54:04 -0700

    Hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - this is the main function
- *
- * Return: always 0
+ * main - A program that prints a line with put function
+ * Return: 0 (success)
  */
-
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);	       
+puts("\"Programming is like building a multilingual puzzle");
+return (0);	       
 }
